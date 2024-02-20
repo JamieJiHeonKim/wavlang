c6d62af2dbe049ef832fbf4e64b11ee53c70f53a
JamieJiHeonKim <jamiejiheonkim@gmail.com>
2024-02-19 23:11:12 -0500

    More tasks added for the future

--- frontend/src/pages/UpdatesPage.js
+++ frontend/src/pages/UpdatesPage.js
@@ -3,3 +3,6 @@
 // 2. Transcription service on live (stream) through mic
 // 3. Desktop application to listen to calls and meetings from other desktop applications directly
 // 4. Assembly AI speaker display
+// 5. Hot Topics
+// 6. Most Used Topic and Analysis Types
+// 7. Chat GPT Integration for additional help
